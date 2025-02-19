54c8e359b4502d5d17fd059cabbe2fefe26f3773
vinitaacharya <vinitaacharya2004@gmail.com>
2025-02-19 18:13:50 -0500

    Java file that prints random numbers

---
+++ code.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class code {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println("Random Number: " + random.nextInt(100));
+    }
+}
